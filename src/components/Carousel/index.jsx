d598749621785246d06abd1a99cb0949b8abe5cb
Florian Badenes <f.badenes@orange.fr>
2025-08-26 14:18:40 +0200

    Fix Carousel si le logement n'a qu'une image

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -13,28 +13,49 @@ const Carousel = ({ items }) => {
     setCurrentIndex((prev) => (prev === items.length - 1 ? 0 : prev + 1));
   };
 
-  return (
-    <div className="carousel">
-      <button className="carousel-button carousel-button-prev" onClick={prevSlide}>
-        <img src={ArrowLeft} alt="arrow-left" />
-      </button>
+  if(items.length > 1)
+  {
+    return (
+      <div className="carousel">
+        <button className="carousel-button carousel-button-prev" onClick={prevSlide}>
+          <img src={ArrowLeft} alt="arrow-left" />
+        </button>
 
-      <div className="carousel-container">
-        {items.map((item, index) => (
-          <div
-            className={`carousel-item ${index === currentIndex ? 'active' : ''}`}
-            key={index}
-          >
-            {item}
-          </div>
-        ))}
+        <div className="carousel-container">
+          {items.map((item, index) => (
+            <div
+              className={`carousel-item ${index === currentIndex ? 'active' : ''}`}
+              key={index}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+
+        <button className="carousel-button carousel-button-next" onClick={nextSlide}>
+          <img src={ArrowRight} alt="arrow-right" />
+        </button>
       </div>
+    );
+  }
+  else{
+    return (
+      <div className="carousel">
+        <div className="carousel-container">
+          {items.map((item, index) => (
+            <div
+              className={`carousel-item ${index === currentIndex ? 'active' : ''}`}
+              key={index}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
-      <button className="carousel-button carousel-button-next" onClick={nextSlide}>
-        <img src={ArrowRight} alt="arrow-right" />
-      </button>
-    </div>
-  );
+  
 };
 
 export default Carousel;
--- src/pages/Fiche-logement/index.jsx
+++ src/pages/Fiche-logement/index.jsx
@@ -6,15 +6,13 @@ import { useParams } from "react-router-dom";
 function Logement() {
 
   const { id } = useParams();
-  // Choisir un logement par ID (par exemple depuis une route ou dynamiquement)
-  const logementId =  'b9123946';
   const logement = data.find(item => item.id === id);
 
   if (!logement) return <p>Logement non trouvé</p>;
 
   const slides = logement.pictures.map((url, index) => (
     <img src={url} alt={`Slide ${index + 1}`} key={index} />
-  ));
+  )); 
   
   return (
     <div>
