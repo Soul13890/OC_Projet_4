318a5a703038f4f112fba7e9bb8653db3b47f022
Florian Badenes <f.badenes@orange.fr>
2025-09-16 12:39:40 +0200

    Ajout scrollToTop ficheLogement

--- src/pages/Fiche-logement/index.jsx
+++ src/pages/Fiche-logement/index.jsx
@@ -4,11 +4,16 @@ import InfoLogement from "../../components/InfoLogement"
 import data from "../../datas/data.json"
 import { useParams } from "react-router-dom";
 import { Navigate } from "react-router-dom";
+import { useEffect } from 'react';
 
 function Logement() {
 
   const { id } = useParams();
   const logement = data.find(item => item.id === id);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   if (!logement){
     return <Navigate to="/error" replace/>
@@ -16,7 +21,7 @@ function Logement() {
 
   const slides = logement.pictures.map((url, index) => (
     <img src={url} alt={`Slide ${index + 1}`} key={index} />
-  )); 
+  ));
   
   return (
     <div>
