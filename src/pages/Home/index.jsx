422ff348e263c6a5aee04cc726446189327ff67f
Florian Badenes <f.badenes@orange.fr>
2025-09-16 12:44:37 +0200

    Ajout scrollToTop page d'accueil

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,13 @@
 import Banner from "../../components/Banner"
 import Header from "../../components/Header"
 import Gallery from "../../components/Gallery"
+import { useEffect } from "react";
 
 function Home() {  
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (    
     <div>
